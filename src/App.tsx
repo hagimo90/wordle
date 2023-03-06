782ea8bb95cf861974aba21d5a89047b38dd9aa4
hagimo90 <hagimo90@gmail.com>
2023-03-06 04:48:23 +0100

    changes in package.json and adding Zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import WordRow from './WordRow'
-
+import { useState } from 'react'
 export default function App() {
+  const [guess, setGuess] = useState('')
   return (
     <div className="mx-auto w-96">
       <header className="my-2 border-b border-gray-500 pb-2">
         <h1 className="text-center text-4xl">Reacdle</h1>
+        <div>
+          <input
+            type="text"
+            className="w-full border-2 border-gray-500 p-2"
+            value={guess}
+            placeholder="Put your guess here"
+            onChange={(e) => setGuess(e.target.value)}
+          />
+        </div>
       </header>
       <main className="grid grid-rows-6 gap-4">
         <WordRow letters="Hello" />
